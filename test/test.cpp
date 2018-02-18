1dbf00377afa22ab7d9411b7f703fc751f4e44fc
misebox <misebox@gmail.com>
2018-02-18 14:48:45 +0900

    Simplify tests with fixtures

--- test/test.cpp
+++ test/test.cpp
@@ -4,197 +4,152 @@ extern "C" {
 #include <cstddef>
 }
 
-class fixtureName : public ::testing::Test {
+class fixture_none: public ::testing::Test {
+};
+
+class for_vec: public ::testing::Test {
 protected:
+    vec_t f_empty_;
+    vec_t f_three_;
+    int64_t f_a_, f_b_, f_c_;
+
     virtual void SetUp(){
+        f_empty_ = vec_new();
+        f_a_ = 10, f_b_ = 20, f_c_ = 30;
+        f_three_ = vec_new();
+        vec_push(f_three_, &f_a_);
+        vec_push(f_three_, &f_b_);
+        vec_push(f_three_, &f_c_);
     }
     virtual void TearDown(){
+        vec_free(&f_empty_);
+        vec_free(&f_three_);
     }
 };
 
-TEST_F(fixtureName, test_vec_new_and_free)
+TEST_F(fixture_none, test_vec_new_and_free)
 {
     vec_t v = vec_new();
     ASSERT_NE((vec_t) NULL, v);
     EXPECT_EQ(0, v->length);
     EXPECT_EQ(8, v->reserved);
     vec_free(&v);
-    EXPECT_EQ((vec_t) NULL, v);
+    ASSERT_EQ((vec_t) NULL, v);
 }
 
-TEST_F(fixtureName, test_vec_push_and_get)
+TEST_F(fixture_none, test_vec_push_and_get)
 {
     vec_t v = vec_new();
-    ASSERT_NE((vec_t) NULL, v);
-    int64_t a = 1, b = 2, c = 3;
-    EXPECT_TRUE(vec_push(v, &a));
-    EXPECT_TRUE(vec_push(v, &b));
-    EXPECT_TRUE(vec_push(v, &c));
-    EXPECT_EQ(&a, vec_get(v, 0));
-    EXPECT_EQ(&b, vec_get(v, 1));
-    EXPECT_EQ(&c, vec_get(v, 2));
+    int64_t f_a_ = 1, f_b_ = 2, f_c_ = 3;
+    EXPECT_TRUE(vec_push(v, &f_a_));
+    EXPECT_TRUE(vec_push(v, &f_b_));
+    EXPECT_TRUE(vec_push(v, &f_c_));
+    EXPECT_EQ(&f_a_, vec_get(v, 0));
+    EXPECT_EQ(&f_b_, vec_get(v, 1));
+    EXPECT_EQ(&f_c_, vec_get(v, 2));
     EXPECT_EQ(NULL, vec_get(v, 3));
     EXPECT_EQ(3, v->length);
     EXPECT_EQ(8, v->reserved);
     vec_free(&v);
-    EXPECT_EQ((vec_t) NULL, v);
 }
 
-TEST_F(fixtureName, test_vec_insert)
+TEST_F(for_vec, test_vec_insert)
 {
-    vec_t v = vec_new();
-    ASSERT_NE((vec_t) NULL, v);
-    int64_t a = 1, b = 2, c = 3, d = 4;
-    EXPECT_TRUE(vec_insert(v, &a, 0));
-    EXPECT_TRUE(vec_insert(v, &c, 1));
-    EXPECT_TRUE(vec_insert(v, &b, 1));
+    vec_t v = f_empty_;
+    int64_t f_a_ = 1, f_b_ = 2, f_c_ = 3, d = 4;
+    EXPECT_TRUE(vec_insert(v, &f_a_, 0));
+    EXPECT_TRUE(vec_insert(v, &f_c_, 1));
+    EXPECT_TRUE(vec_insert(v, &f_b_, 1));
     EXPECT_FALSE(vec_insert(v, &d, 4));
-    EXPECT_EQ(&a, vec_get(v, 0));
-    EXPECT_EQ(&b, vec_get(v, 1));
-    EXPECT_EQ(&c, vec_get(v, 2));
-    EXPECT_EQ(NULL, vec_get(v, 3));
+    EXPECT_EQ(&f_a_, v->items[0]);
+    EXPECT_EQ(&f_b_, v->items[1]);
+    EXPECT_EQ(&f_c_, v->items[2]);
     EXPECT_EQ(3, v->length);
     EXPECT_EQ(8, v->reserved);
-    vec_free(&v);
-    EXPECT_EQ((vec_t) NULL, v);
 }
 
-TEST_F(fixtureName, test_vec_del)
+TEST_F(for_vec, test_vec_del)
 {
-    vec_t v = vec_new();
-    ASSERT_NE((vec_t) NULL, v);
-    int64_t a = 99;
-    EXPECT_TRUE(vec_push(v, &a));
-    EXPECT_EQ(&a, vec_get(v, 0));
+    vec_t v = f_three_;
+    EXPECT_EQ(3, v->length);
+    EXPECT_FALSE(vec_del(v, 3));
+    EXPECT_EQ(3, v->length);
+    EXPECT_TRUE(vec_del(v, 1));
+    EXPECT_EQ(2, v->length);
+    EXPECT_TRUE(vec_del(v, 1));
     EXPECT_EQ(1, v->length);
+    EXPECT_FALSE(vec_del(v, 1));
     EXPECT_TRUE(vec_del(v, 0));
-    EXPECT_EQ(NULL, vec_get(v, 0));
     EXPECT_EQ(0, v->length);
-
-    vec_free(&v);
-    EXPECT_EQ((vec_t) NULL, v);
+    EXPECT_FALSE(vec_del(v, 0));
 }
 
-TEST_F(fixtureName, test_vec_pop)
+TEST_F(for_vec, test_vec_pop)
 {
-    vec_t v = vec_new();
-    ASSERT_NE((vec_t) NULL, v);
-    int64_t a=1, b=2, c=3;
-    EXPECT_TRUE(vec_push(v, &a));
-    EXPECT_TRUE(vec_push(v, &b));
-    EXPECT_TRUE(vec_push(v, &c));
-    EXPECT_EQ(3, v->length);
-    EXPECT_EQ(8, v->reserved);
-    EXPECT_EQ(&c, vec_pop(v));
-    EXPECT_EQ(&b, vec_pop(v));
-    EXPECT_EQ(&a, vec_pop(v));
+    vec_t v = f_three_;
+    EXPECT_EQ(&f_c_, vec_pop(v));
+    EXPECT_EQ(2, v->length);
+    EXPECT_EQ(&f_b_, vec_pop(v));
+    EXPECT_EQ(1, v->length);
+    EXPECT_EQ(&f_a_, vec_pop(v));
+    EXPECT_EQ(0, v->length);
     EXPECT_EQ(NULL, vec_pop(v));
-    EXPECT_EQ(0, v->length);
-    vec_free(&v);
-    EXPECT_EQ((vec_t) NULL, v);
 }
 
-TEST_F(fixtureName, test_reserve_correctly)
+TEST_F(for_vec, test_vec_push_many_items)
 {
-    int64_t reserved = 8;
-    vec_t v = vec_new();
-    ASSERT_NE((vec_t) NULL, v);
-    int64_t a[9] = {99};
-    EXPECT_EQ(8, v->reserved);
-    EXPECT_TRUE(vec_reserve(v, 8));
-    for (int64_t i = 0; i < reserved + 1; i++) {
-        EXPECT_TRUE(vec_push(v, &a[i]));
-        EXPECT_EQ(&a[i], vec_get(v, i));
-    }
-
-    EXPECT_EQ(reserved + 1, v->length);
-    EXPECT_EQ(reserved * 2, v->reserved);
-    vec_free(&v);
-    EXPECT_EQ((vec_t) NULL, v);
-}
-
-TEST_F(fixtureName, test_vec_push_many_items)
-{
-    vec_t v = vec_new();
-    ASSERT_NE((vec_t) NULL, v);
-    int64_t a = 99;
-    int64_t res = 8;
+    vec_t v = f_empty_;
+    int64_t f_a_ = 99;
+    int64_t res = v->reserved;
     for (int64_t i = 0; i < 0x10000; i++) {
-        EXPECT_TRUE(vec_push(v, &a));
-        EXPECT_EQ(&a, vec_get(v, i));
+        EXPECT_TRUE(vec_push(v, &f_a_));
+        EXPECT_EQ(&f_a_, vec_get(v, i));
+        EXPECT_EQ(v->length, i+1);
         if (res < i) {
             res *= 2;
             EXPECT_EQ(res, v->reserved);
         }
     }
-
-    EXPECT_EQ(0x10000, v->length);
-    vec_free(&v);
-    EXPECT_EQ((vec_t) NULL, v);
 }
 
-TEST_F(fixtureName, test_vec_reserve_256K_bytes)
+TEST_F(for_vec, test_vec_reserve_256K_bytes)
 {
-    vec_t v = vec_new();
-    ASSERT_NE((vec_t) NULL, v);
-    int64_t a = 99;
-    int64_t res = 0x10000;
-    EXPECT_TRUE(vec_push(v, &a));
-    EXPECT_EQ(&a, vec_get(v, 0));
+    vec_t v = f_three_;
+    uint64_t length = v->length;
+    uint64_t res = 0x10000;
     EXPECT_TRUE(vec_reserve(v, res));
     EXPECT_EQ(res, v->reserved);
-
-    EXPECT_EQ(1, v->length);
-    EXPECT_EQ(a, 99);
-    EXPECT_EQ(&a, vec_get(v, 0));
-    vec_free(&v);
-    EXPECT_EQ((vec_t) NULL, v);
+    EXPECT_EQ(length, v->length);
+    EXPECT_EQ(&f_a_, v->items[0]);
+    EXPECT_EQ(&f_b_, v->items[1]);
+    EXPECT_EQ(&f_c_, v->items[2]);
 }
 
-TEST_F(fixtureName, test_vec_copy)
+TEST_F(for_vec, test_vec_copy)
 {
-    vec_t v = vec_new();
-    ASSERT_NE((vec_t) NULL, v);
-    int64_t a=1, b=2, c=3;
-    EXPECT_TRUE(vec_push(v, &a));
-    EXPECT_TRUE(vec_push(v, &b));
-    EXPECT_TRUE(vec_push(v, &c));
-    EXPECT_EQ(3, v->length);
-    EXPECT_EQ(8, v->reserved);
-    vec_t v2 = vec_copy(v);
-    // free source vec
-    vec_free(&v);
-    EXPECT_EQ((vec_t) NULL, v);
-    // expect destination vec
-    EXPECT_NE((vec_t) NULL, v2);
-    EXPECT_EQ(&a, vec_get(v2, 0));
-    EXPECT_EQ(&b, vec_get(v2, 1));
-    EXPECT_EQ(&c, vec_get(v2, 2));
-    EXPECT_EQ(3, v2->length);
-    EXPECT_EQ(8, v2->reserved);
-    vec_free(&v2);
-    EXPECT_EQ((vec_t) NULL, v2);
+    vec_t v = f_three_;
+    vec_t v_dst = vec_copy(v);
+    EXPECT_NE(v, v_dst);
+    EXPECT_NE(v->items, v_dst->items);
+    EXPECT_NE((vec_t) NULL, v_dst);
+    EXPECT_EQ(&f_a_, vec_get(v_dst, 0));
+    EXPECT_EQ(&f_b_, vec_get(v_dst, 1));
+    EXPECT_EQ(&f_c_, vec_get(v_dst, 2));
+    EXPECT_EQ(v->length, v_dst->length);
+    EXPECT_EQ(v->reserved, v_dst->reserved);
+    vec_free(&v_dst);
+    EXPECT_EQ((vec_t) NULL, v_dst);
 }
 
-TEST_F(fixtureName, test_vec_copy_slice)
+TEST_F(for_vec, test_vec_copy_slice)
 {
-    vec_t v = vec_new();
-    ASSERT_NE((vec_t) NULL, v);
-    int64_t a=1, b=2, c=3;
-    EXPECT_TRUE(vec_push(v, &a));
-    EXPECT_TRUE(vec_push(v, &b));
-    EXPECT_TRUE(vec_push(v, &c));
-    EXPECT_EQ(3, v->length);
-    EXPECT_EQ(8, v->reserved);
-    vec_t v2 = vec_copy_slice(v, 1, 2);
-    // free source vec
-    vec_free(&v);
-    EXPECT_EQ((vec_t) NULL, v);
-    // expect destination vec
-    EXPECT_NE((vec_t) NULL, v2);
-    EXPECT_EQ(&b, vec_get(v2, 0));
-    EXPECT_EQ(1, v2->length);
-    EXPECT_EQ(8, v2->reserved);
-    vec_free(&v2);
-    EXPECT_EQ((vec_t) NULL, v2);
+    vec_t v = f_three_;
+    vec_t v_dst = vec_copy_slice(v, 1, 2);
+    EXPECT_NE(v, v_dst);
+    EXPECT_NE((vec_t) NULL, v_dst);
+    EXPECT_EQ(&f_b_, vec_get(v_dst, 0));
+    EXPECT_EQ(1, v_dst->length);
+    EXPECT_EQ(8, v_dst->reserved);
+    vec_free(&v_dst);
+    EXPECT_EQ((vec_t) NULL, v_dst);
 }
