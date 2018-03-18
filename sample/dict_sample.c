8bc2cfc566abc05b1aabaf9d722b165700ba323a
misebox <misebox@gmail.com>
2018-03-18 16:19:48 +0900

    Improve the dict sample

--- sample/dict_sample.c
+++ sample/dict_sample.c
@@ -14,6 +14,17 @@ static char *fruits[] = {
     "Dates",
 };
 
+void
+show_dict(pdict d) {
+    printf("dict: %p (count: %3u, size: %3u)\n", d, d->count, d->size);
+    for (dict_size_t i = 0; i < d->size; i++) {
+        pdict_item slot = (d->items + i);
+        if (slot->key != NULL)
+            printf("[hash: %u, key: %s, value: %p (%s)]\n", slot->hash, slot->key->bytes, slot->value, slot->value);
+    }
+}
+
+
 int main() {
     for (dict_size_t size = 16; size <= 256; size <<= 1) {
         for (uint8_t i = 0; i < 4; i++) {
@@ -23,5 +34,17 @@ int main() {
         }
         printf("\n");
     }
+    pdict pd = dict_new(100);
+    pu8 key1 = u8_new("This is a key.");
+    pu8 key2 = u8_new("This is a key.2222");
+    dict_size_t addr1 = dict_set(pd, key1, (voidptr_t)"apple");
+    dict_size_t addr2 = dict_set(pd, key2, (voidptr_t)"banana");
+    const char *val1 = dict_get(pd, key1);
+    const char *val2 = dict_get(pd, key2);
+    show_dict(pd);
+    u8_free(&key1);
+    u8_free(&key2);
+    dict_free(&pd);
+
     return 0;
 }
